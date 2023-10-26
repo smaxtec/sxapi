541cdc680b9bcedfd6cc7c0e40387c1abf35212c
Marco Julian Moser <hyper_chrome@gmx.at>
2023-10-26 09:08:21 +0000

    added groups endpoint

---
+++ src/sxapi/publicV2/groups.py
@@ -0,0 +1,73 @@
+class Groups:
+    """
+    This Class represents the /groups endpoint of the PublicAPIV2.
+    https://api.smaxtec.com/api/v2/
+    """
+
+    def __init__(self, api=None):
+        self.api = api
+        self.path_suffix = "/groups"
+
+    def put_actions(self, group_id, **kwargs):
+        """
+        Add an action to a group.
+
+        Args:
+            group_id (str): ID of the group which should be updated.
+            **kwargs: Optional parameters of the API call.
+                Find supported parameters under
+                https://api.smaxtec.com/api/v2/
+
+        Returns:
+            dict: Response of API call. Updated group on success, error message else.
+        """
+        params = {}
+
+        for k, v in kwargs.items():
+            params[k] = v
+
+        url_suffix = self.path_suffix + f"/{group_id}/actions"
+        self.api.put(url_suffix, json=params)
+
+    def get_actions(self, group_id, **kwargs):
+        """
+        Get all actions of a group.
+
+        Args:
+            group_id (str): ID of the group which should be updated.
+            **kwargs: Optional parameters of the API call.
+                Find supported parameters under
+                https://api.smaxtec.com/api/v2/
+
+        Returns:
+            dict: Response of API call. Updated group on success, error message else.
+        """
+        params = {}
+
+        for k, v in kwargs.items():
+            params[k] = v
+
+        url_suffix = self.path_suffix + f"/{group_id}/actions"
+        return self.api.get(url_suffix, json=params)
+
+    def delete_actions(self, group_id, action_id, **kwargs):
+        """
+        Delete an action from a group.
+
+        Args:
+            group_id (str): ID of the group which should be updated.
+            action_id (str): ID of the action which should be deleted.
+            **kwargs: Optional parameters of the API call.
+                Find supported parameters under
+                https://api.smaxtec.com/api/v2/
+
+        Returns:
+            dict: Response of API call. Updated group on success, error message else.
+        """
+        params = {}
+
+        for k, v in kwargs.items():
+            params[k] = v
+
+        url_suffix = self.path_suffix + f"/{group_id}/actions/{action_id}"
+        return self.api.delete(url_suffix, json=params)
