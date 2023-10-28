6f379bb8f795e9c158b58965c5db0ce00f12ecb5
Moser Marco Julian <marco.moser@smaxtec.com>
2023-10-28 15:20:53 +0200

    added events endpoint

---
+++ src/sxapi/publicV2/events.py
@@ -0,0 +1,77 @@
+class Events:
+    """
+    This Class represents the /events endpoint of the PublicAPIV2.
+    https://api.smaxtec.com/api/v2/
+    """
+
+    def __init__(self, api=None):
+        self.api = api
+        self.path_suffix = "/events"
+
+    def get(self, organisation_id, **kwargs):
+        """Get all events of an organisation.
+
+        Args:
+            organisation_id (str): ID of the organisation the events
+                should be retrieved for
+            **kwargs: Optional parameters of the API call.
+                Find supported parameters under
+                https://api.smaxtec.com/api/v2/
+
+        Returns:
+            dict: Response of API call. List of events on success,
+                error message else.
+
+        """
+        params = {
+            "organisation_id": organisation_id,
+        }
+
+        for k, v in kwargs.items():
+            params[k] = v
+
+        return self.api.get(self.path_suffix, json=params)
+
+    def get_by_id(self, event_id, **kwargs):
+        """Get one event by ID.
+
+        Args:
+            event_id (str): ID of the desired event
+            **kwargs: Optional parameters of the API call.
+                Find supported parameters under
+                https://api.smaxtec.com/api/v2/
+
+        Returns:
+            dict: Response of API call. Event on success, error message else.
+
+        """
+        params = {
+            "event_id": event_id,
+        }
+        for k, v in kwargs.items():
+            params[k] = v
+
+        url_suffix = self.path_suffix + "/by_id"
+        return self.api.get(url_suffix, json=params)
+
+    def get_categories(self, **kwargs):
+        """Get all event categories.
+            If no user id or organisation id in the parameter
+            a Bad Request Error will be returned!
+
+        Args:
+            **kwargs: Optional parameters of the API call.
+                Find supported parameters under
+                https://api.smaxtec.com/api/v2/
+
+        Returns:
+            dict: Response of API call. List of event categories on success,
+                error message else.
+
+        """
+        params = {}
+        for k, v in kwargs.items():
+            params[k] = v
+
+        url_suffix = self.path_suffix + "/categories"
+        return self.api.get(url_suffix, json=params)
---
+++ tests/test_publicV2/test_events.py
@@ -0,0 +1,46 @@
+import mock
+
+from sxapi.publicV2 import PublicAPIV2
+
+@mock.patch("sxapi.publicV2.PublicAPIV2.get")
+def test_get(get_mock):
+    test_api = PublicAPIV2()
+    test_api.events.get("test_organisation_id", kwarg1="kwarg1")
+
+    call_args = get_mock.call_args_list[0]
+
+    assert get_mock.call_count == 1
+    assert call_args.args[0] == "/events"
+    assert call_args.kwargs["json"] == {
+        "organisation_id": "test_organisation_id",
+        "kwarg1": "kwarg1",
+    }
+
+
+@mock.patch("sxapi.publicV2.PublicAPIV2.get")
+def test_get_by_id(get_mock):
+    test_api = PublicAPIV2()
+    test_api.events.get_by_id("test_event_id", kwarg1="kwarg1")
+
+    call_args = get_mock.call_args_list[0]
+
+    assert get_mock.call_count == 1
+    assert call_args.args[0] == "/events/by_id"
+    assert call_args.kwargs["json"] == {
+        "event_id": "test_event_id",
+        "kwarg1": "kwarg1",
+    }
+
+
+@mock.patch("sxapi.publicV2.PublicAPIV2.get")
+def test_get_categories(get_mock):
+    test_api = PublicAPIV2()
+    test_api.events.get_categories(kwarg1="kwarg1")
+
+    call_args = get_mock.call_args_list[0]
+
+    assert get_mock.call_count == 1
+    assert call_args.args[0] == "/events/categories"
+    assert call_args.kwargs["json"] == {
+        "kwarg1": "kwarg1", 
+    }
